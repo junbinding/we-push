7970f2f9855eedc70936a66b310c4177d3441dc5
dingjunbin <junbinding@gmail.com>
2020-09-28 10:03:59 +0800

    env: add dependencies

--- config/config.default.js
+++ config/config.default.js
@@ -18,13 +18,31 @@ module.exports = appInfo => {
   // add your middleware config here
   config.middleware = [];
 
-  // add your user config here
-  const userConfig = {
-    // myAppName: 'egg',
+  // 安全配置
+  config.security = {
+    csrf: {
+      ignoreJSON: true,
+    },
+  };
+
+  // 微信配置
+  config.wechat = {
+    appid: 'wx6b49cff143ce2371',
+    appsecret: '39a81b8876349066c1001e68ddcd3901',
+    token: '76f252ff44508b5c7028526978dc37e4',
+    encodingAESKey: 'ZhgHzjvrYfhrYJmaoglW8gEzBiKckP53fWfrK2VgAWA',
+    checkSignature: true,
+    pushKey: 'wx_push_',
+    pushTtl: 48 * 3600,
+    templateWarning: 'gcqFueEFJyXjhdzNyGkzdNkcsWsKQfyIDAG6f1wuLe4',
+  };
+
+  // 日志配置
+  config.logger = {
+    outputJSON: true,
   };
 
   return {
     ...config,
-    ...userConfig,
   };
 };
